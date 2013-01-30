2fc348585fe1556c4d782c6c7c496575d68b55c7
ketola <sauli.ketola@mbnet.fi>
2013-01-30 20:58:34 +0200

    initial commit

---
+++ PlanningPoker/gen/ketola/mobile/phonegap/planningpoker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ketola.mobile.phonegap.planningpoker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
